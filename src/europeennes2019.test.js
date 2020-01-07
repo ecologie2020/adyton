f189b90d2e609ddd8ae7050b2b38d8abda8b3643
Matti Schneider <2020@mattischneider.fr>
2020-01-07 09:53:28 +0100

    Correct bureaux order for integer values

--- src/europeennes2019.test.js
+++ src/europeennes2019.test.js
@@ -7,7 +7,7 @@ let fileContents,
 	parsedData;
 
 beforeAll(done => {
-	fs.readFile(`${__dirname}/../test/mock/europeennes-2019-nice-head2.csv`, (err, contents) => {
+	fs.readFile(`${__dirname}/../test/mock/europeennes-2019-nice-mock.csv`, (err, contents) => {
 		if (err)
 			throw err;
 
@@ -24,7 +24,7 @@ describe('Européennes 2019', () => {
 				throw err;
 
 			expect(bureaux).toBeInstanceOf(Array);
-			expect(bureaux).toHaveLength(2);
+			expect(bureaux).toHaveLength(3);
 			expect(bureaux[0]).toHaveProperty('Inscrits', '784');
 
 			parsedData = bureaux;
@@ -37,8 +37,9 @@ describe('Européennes 2019', () => {
 		let sortedBureaux = europeennes2019.sortBureauxBy(parsedData, 'Inscrits');
 
 		expect(sortedBureaux).toBeInstanceOf(Array);
-		expect(sortedBureaux).toHaveLength(2);
+		expect(sortedBureaux).toHaveLength(3);
 		expect(sortedBureaux[0]).toHaveProperty('Inscrits', '793');
 		expect(sortedBureaux[1]).toHaveProperty('Inscrits', '784');
+		expect(sortedBureaux[2]).toHaveProperty('Inscrits', '79');
 	});
 });

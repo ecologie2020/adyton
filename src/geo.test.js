67228e2cfb6fe8c14e90ab89d8741d97552235d2
Matti Schneider <2020@mattischneider.fr>
2020-01-10 16:36:08 +0100

    Minor test refactor

--- src/geo.test.js
+++ src/geo.test.js
@@ -7,6 +7,55 @@ describe('Geojson', () => {
 	test('is generated', () => {
 		let data = PARSED_BUREAUX_DATA.map(bureau => { return { [bureau.Bureau]: bureau.Inscrits } });
 
-		expect(geo.geojsonFromBureaux(data)).toEqual({"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[7.25892245,43.69524528]},"properties":{"id":"102","name":"MEYERBEER","valeur":"784"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[7.25892245,43.69524528]},"properties":{"id":"101","name":"MASSENA","valeur":"793"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[7.23342081,43.68530115]},"properties":{"id":"203","name":"SAINTE HELENE","valeur":"79"}}]});
+		expect(geo.geojsonFromBureaux(data)).toEqual({
+			"type": "FeatureCollection",
+			"features": [
+				{
+					"type": "Feature",
+					"geometry": {
+						"type": "Point",
+						"coordinates": [
+							7.25892245,
+							43.69524528
+						]
+					},
+					"properties": {
+						"id": "102",
+						"name": "MEYERBEER",
+						"valeur": "784"
+					}
+				},
+				{
+					"type": "Feature",
+					"geometry": {
+						"type": "Point",
+						"coordinates": [
+							7.25892245,
+							43.69524528
+						]
+					},
+					"properties": {
+						"id": "101",
+						"name": "MASSENA",
+						"valeur": "793"
+					}
+				},
+				{
+					"type": "Feature",
+					"geometry": {
+						"type": "Point",
+						"coordinates": [
+							7.23342081,
+							43.68530115
+						]
+					},
+					"properties": {
+						"id": "203",
+						"name": "SAINTE HELENE",
+						"valeur": "79"
+					}
+				}
+			]
+		});
 	});
 });

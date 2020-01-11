b2e478ba247f344ce5d92006127d476ff94285ca
Matti Schneider <2020@mattischneider.fr>
2020-01-11 16:40:12 +0100

    Normalise heatmap values
    Visualise relative rather than absolute values

--- front/js/loadHeatMap.js
+++ front/js/loadHeatMap.js
@@ -2,17 +2,19 @@ function loadHeatMap(sourceFileId, gradient) {
 	return fetch(`/dist/${sourceFileId}.geojson`)
 		.then(potentialScores => potentialScores.json())
 		.then(potentialScores => {
-			let maxScore = Math.max(...potentialScores.features.map(bureau => bureau.properties.valeur));
+			let values = potentialScores.features.map(bureau => bureau.properties.valeur);
+			let min = Math.min(...values),
+				max = Math.max(...values);
+
 			let heatMapData = potentialScores.features.map(bureau => [
 				bureau.geometry.coordinates[1],
 				bureau.geometry.coordinates[0],
-				bureau.properties.valeur,  // the heatmap plugin uses elevation as heat intensity, on a 0-1 scale
+				(bureau.properties.valeur - min) / (max - min),  // the heatmap plugin uses elevation as heat intensity, on a 0-1 scale
 			]);
 
 			return L.heatLayer(heatMapData, {
 				blur: 25,
 				gradient,
-				max: maxScore,
 				maxZoom: 16,
 				radius: 50,
 			});

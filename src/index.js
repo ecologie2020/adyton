d2352bd552e0b2437546443c8fda138ca731209c
Matti Schneider <2020@mattischneider.fr>
2020-01-13 17:34:25 +0100

    Generate maps for both Menton and Nice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 
 const europeennes2019 = require('./europeennes2019');
-const geojsonFromBureaux = require('./normalise-bureaux/nice-06-088').geojsonFromBureaux;
+const mapNice = require('./normalise-bureaux/nice-06-088').geojsonFromBureaux;
+const mapMenton = require('./normalise-bureaux/menton-06-083').geojsonFromBureaux;
 const reportsMappings = require('../data/europeennes-2019-reports.json').écologie;
 
 europeennes2019.load(`${__dirname}/../data/europeennes-2019-menton.csv`)
 	.then(europeennes2019.parse)
 	.then(bureaux => {
-		fs.writeFile('dist/menton-06-083.mock.js', JSON.stringify(bureaux), err => {
+		let bureauxByAbstention = bureaux.map(bureau => { return { [bureau.Bureau]: bureau['% Abs/Ins'] } });
+
+		let averageReportsMapping = europeennes2019.mergeMappings(reportsMappings);
+		let potentialScores = europeennes2019.getPotentialScoresForMapping(bureaux, averageReportsMapping);
+
+		fs.writeFile('dist/bureauxByAbstention-menton.geojson', JSON.stringify(mapMenton(bureauxByAbstention)), err => {
 			if (err) throw err;
-			console.log('Wrote dist/menton-06-083.mock.js');
+			console.log('Wrote bureauxByAbstention-menton.geojson');
+		});
+
+		fs.writeFile('dist/potentialScores-menton.geojson', JSON.stringify(mapMenton(potentialScores)), err => {
+			if (err) throw err;
+			console.log('Wrote potentialScores-menton.geojson');
 		});
 	});
 
@@ -21,24 +32,16 @@ europeennes2019.load(`${__dirname}/../data/europeennes-2019-nice.csv`)
 		let bureauxByAbstention = europeennes2019.sortBureauxBy(bureaux, '% Abs/Ins');
 		bureauxByAbstention = bureauxByAbstention.map(bureau => { return { [bureau.Bureau]: bureau['% Abs/Ins'] } });
 
-		let bureauxByEELVScore = europeennes2019.sortBureauxBy(bureaux, '% Voix/Exp EUROPE ÉCOLOGIE');
-		bureauxByEELVScore = bureauxByEELVScore.map(bureau => { return { [bureau.Bureau]: bureau['% Voix/Exp EUROPE ÉCOLOGIE'] } });
-
 		let averageReportsMapping = europeennes2019.mergeMappings(reportsMappings);
 		let potentialScores = europeennes2019.getPotentialScoresForMapping(bureaux, averageReportsMapping);
 
-		fs.writeFile('dist/bureauxByAbstention.geojson', JSON.stringify(geojsonFromBureaux(bureauxByAbstention)), err => {
+		fs.writeFile('dist/bureauxByAbstention-nice.geojson', JSON.stringify(mapNice(bureauxByAbstention)), err => {
 			if (err) throw err;
-			console.log('Wrote bureauxByAbstention.geojson');
+			console.log('Wrote bureauxByAbstention-nice.geojson');
 		});
 
-		fs.writeFile('dist/bureauxByEELVScore.geojson', JSON.stringify(geojsonFromBureaux(bureauxByEELVScore)), err => {
+		fs.writeFile('dist/potentialScores-nice.geojson', JSON.stringify(mapNice(potentialScores)), err => {
 			if (err) throw err;
-			console.log('Wrote bureauxByEELVScore.geojson');
-		});
-
-		fs.writeFile('dist/potentialScores.geojson', JSON.stringify(geojsonFromBureaux(potentialScores)), err => {
-			if (err) throw err;
-			console.log('Wrote potentialScores.geojson');
+			console.log('Wrote potentialScores-nice.geojson');
 		});
 	});
